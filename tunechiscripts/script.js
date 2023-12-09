1b47e407de5a07e405c7c828f48e12daa15d99d7
Tryb3 <139131887+Tryb3@users.noreply.github.com>
2023-12-09 12:43:51 +0100

    Add files via upload

---
+++ tunechiscripts/script.js
@@ -0,0 +1,12 @@
+function discoverNow() {
+    // Add JavaScript logic for the "Discover Now" button
+    alert("connect wallet first");
+    // You can redirect the user or perform other actions as needed
+}
+
+function claimAirdrop() {
+    // Add JavaScript logic for claiming the airdrop
+    // This could include form validation, API calls, etc.
+    alert("Airdrop claimed successfully!");
+    // You can redirect the user or perform other actions as needed
+}
